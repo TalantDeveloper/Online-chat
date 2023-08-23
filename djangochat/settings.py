c76b826e9f2a1ef3084e70e157331108633dc2f6
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2023-08-23 13:34:49 +0500

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -87,13 +87,23 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat',
+        'USER': 'chat',
+        'PASSWORD': '@Botirjon06',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
